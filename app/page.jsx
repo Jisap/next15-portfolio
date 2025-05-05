ace6c5fd22a2e138c0a96ed5b993c65e1959b41d
Jorge Matos <jisapjisap@gmail.com>
2025-05-05 18:54:17 +0200

    styles & assets

--- app/page.jsx
+++ app/page.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='py-24'>
-      <h1 className='h1'>
-        Lorem, ipsum
-      </h1>
+    <div>
+      Home
     </div>
   )
 }
