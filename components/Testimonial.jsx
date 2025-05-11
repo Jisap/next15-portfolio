d90c6851836363bfa2b5603a1a839a270957ea3d
Jorge Matos <jisapjisap@gmail.com>
2025-05-11 20:01:01 +0200

    Testimonial

--- components/Testimonial.jsx
+++ components/Testimonial.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+"use client"
+
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Autoplay } from 'swiper'
+import "swiper/css"
+import "swiper/css/pagination"
+import { ImQuotesLeft } from "react-icons/im"
+
+const testimonial = [
+  {
+    message: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.",
+    name: "John Doe",
+  },
+  {
+    message: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.",
+    name: "Mary Doe",
+  },
+  {
+    message: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.",
+    name: "Jake Doe",
+  },
+]
+
 
 const Testimonial = () => {
   return (
-    <div>Testimonial</div>
+    <Swiper 
+      modules={{ Autoplay }} 
+      loop={true}
+      autoplay={{ delay: 4000, disableOnInteraction: false }} 
+      className='w-full max-w-[310px] md:max-w-[520px] bg-secondary rounded-lg'
+    >
+      {testimonial.map((person, index) => {
+        return (
+          <SwiperSlide key={index}>
+            <div className='flex px-8 py-6 gap-8'>
+              <ImQuotesLeft className='hidden xl:flex text-8xl text-accent' />
+              <div className='flex flex-col gap-2'>
+                <p>
+                  {person.message}
+                </p>
+                <p className='self-end text-accent font-semibold'>
+                  {person.name}
+                </p>
+              </div>
+            </div>
+          </SwiperSlide>
+        )
+      })}
+    </Swiper>
   )
 }
 
