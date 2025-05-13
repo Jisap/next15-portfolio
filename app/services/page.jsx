ad9a82883ec5f287435db9a1eb87444450006f74
Jorge Matos <jisapjisap@gmail.com>
2025-05-13 13:31:39 +0200

    services

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,6 +1,43 @@
 "use client"
 
 import { motion } from "framer-motion"
+import { Swiper, SwiperSlide } from "swiper/react"
+import { Pagination } from "swiper/modules"
+import "swiper/css"
+import "swiper/css/pagination"
+import Image from "next/image"
+import {
+  MdOutlineArrowOutward,
+  MdArrowRightAlt,
+} from "react-icons/md"
+
+const services = [
+  {
+    icon: "/assets/services/design.svg",
+    href: "",
+    title: "Website Interface Design",
+  },
+  {
+    icon: "/assets/services/frontend.svg",
+    href: "",
+    title: "Frontend Development",
+  },
+  {
+    icon: "/assets/services/backend.svg",
+    href: "",
+    title: "Backend Development",
+  },
+  {
+    icon: "/assets/services/seo.svg",
+    href: "",
+    title: "Search Engine Optimization",
+  },
+  {
+    icon: "/assets/services/video.svg",
+    href: "",
+    title: "Video Production",
+  },
+]
 
 const Services = () => {
   return (
@@ -10,8 +47,44 @@ const Services = () => {
         opacity: 1,
         transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
       }}
+      className="h-screen flex items-center"
     >
-      Services
+      <div className="container mx-auto w-full flex flex-col gap-16">
+        <div className="flex flex-col xl:flex-row justify-between items-start xl:items-center gap-8">
+          <h2 className="h2 max-w-[480px] text-left xl:mb-0">
+            Custom <span className="text-accent">Web Solutions</span> to Boost Your Business
+          </h2>
+
+          <button className="btn btn-lg btn-accent flex gap-2">
+            All Services <MdArrowRightAlt className="text-2xl" />
+          </button>
+        </div>
+
+        <Swiper 
+          spaceBetween={30} 
+          slidesPerView={1}
+          breakpoints={{
+            640: { slidesPerView: 2 },
+            1024: { slidesPerView: 3 },
+          }}
+        >
+          {services.map((item, index) => {
+            return (
+              <SwiperSlide key={index}>
+                <div className="bg-secondary/90 w-full h-[284px] rounded-[20px] px-[30px] py-[40px] flex flex-col justify-between">
+                  <Image 
+                    src={item.icon}
+                    width={48}
+                    height={48}
+                    alt={item.title}
+                    className=""
+                  />
+                </div>
+              </SwiperSlide>
+            )
+          })}
+        </Swiper>
+      </div>
     </motion.div>
   )
 }
